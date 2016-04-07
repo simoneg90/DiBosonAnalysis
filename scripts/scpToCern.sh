1eca1c89dc890d1be1fd3ceda8c8f80aaef13153
simoneg90 <gelli.simone@alice.it>
2016-04-07 18:27:27 +0200

    added btag infos (loose, medium, tight) for ak04 and subjet, q-Gen and ak04-subjet DR, script to copy in the www folder of Cern

--- scripts/scpToCern.sh
+++ scripts/scpToCern.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-#USAGE ./scripts/scpToCern.sh whatToCopy whereIfYouWant
+#USAGE ./scripts/scpToCern.sh user whatToCopy whereIfYouWant
 #$2 can be omitted!
+
+userLetter="$(echo $1 | head -c 1)"
 
 echo "+++++++++++++++++"
 echo "Hi there!"
-echo "I'm going to copy ${1} here /afs/cern.ch/user/s/sgelli/www/${2}"
+echo "I'm going to copy ${2} here /afs/cern.ch/user/${userLetter}/${1}/www/${3}"
 echo "Remember to add \'\' before and after what you want to copy"
 echo "Are you sure???"
 echo "+++++++++++++++++"
 echo "[Enter to confirm, Ctrl+C to exit]"
 read
-scp -r ${1} sgelli@lxplus.cern.ch:/afs/cern.ch/user/s/sgelli/www/${2}
+
+scp -r ${2} ${1}@lxplus.cern.ch:/afs/cern.ch/user/${userLetter}/${1}/www/${3}
+scp ${1}@lxplus.cern.ch:/afs/cern.ch/user/${userLetter}/${1}/www/index.php ${1}@lxplus.cern.ch:/afs/cern.ch/user/${userLetter}/${1}/www/${3}
 
